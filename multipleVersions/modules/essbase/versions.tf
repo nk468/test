db25e646d6a34c2e48215e1238c993ee621e2c4b
Nandan Kandregula <nandan.kandregula@oracle.com>
2020-12-22 19:59:45 -0500

    test

---
+++ multipleVersions/modules/essbase/versions.tf
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ */
+
+terraform {
+  required_version = "< 0.12"
+}
+
+
---
+++ multipleVersions/tfconfig.tf
@@ -0,0 +1,19 @@
+variable "tenancy_ocid" {}
+variable "compartment_ocid" {}
+variable "region" {}
+variable "display_name_prefix" {}
+terraform {
+  required_version = ">= 0.12.0"
+}
+
+provider "oci" {
+  tenancy_ocid = "${var.tenancy_ocid}"
+  region       = "${var.region}"
+}
+
+resource "oci_core_virtual_network" "vcn1" {
+  cidr_block     = "10.0.0.0/16"
+  dns_label      = "vcn1"
+  compartment_id = "${var.compartment_ocid}"
+  display_name   = "${var.display_name_prefix}VCN1-1-orch-plugin-tests"
+}
